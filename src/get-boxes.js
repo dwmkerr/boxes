9e0ed6394c4d396881ed83106e5e999607fc4feb
Dave Kerr <dwmkerr@gmail.com>
2023-11-10 13:57:11 -0800

    fix: remove hard coded aws region/profile

--- src/get-boxes.js
+++ src/get-boxes.js
@@ -5,6 +5,38 @@ import {
   StopInstancesCommand,
 } from "@aws-sdk/client-ec2";
 
+export async function getBoxes() {
+  const client = new EC2Client();
+
+  const instancesResponse = await client.send(
+    new DescribeInstancesCommand({
+      IncludeAllInstances: true,
+      Filters: [
+        {
+          Name: "tag:boxes.boxid",
+          Values: ["*"],
+        },
+      ],
+    }),
+  );
+
+  const instances = instancesResponse.Reservations.flatMap((r) => r.Instances);
+
+  //  We filter out terminated boxes (otherwise we can get multiple boxes with
+  //  the same ID, e.g. if a user quickly terminates and recreates a box).
+  const validInstances = instances.filter((i) => i.State.Name !== "terminated");
+
+  const boxes = validInstances.map((i) => ({
+    boxId: getTagValOr(i.Tags, "boxes.boxid", null),
+    instanceId: i.InstanceId,
+    name: nameFromTags(i.Tags),
+    status: i.State.Name,
+    instance: i,
+  }));
+
+  return boxes;
+}
+
 const getTagValOr = (tags, tagName, fallback) => {
   return tags.reduce((acc, val) => {
     return val.Key == tagName ? val.Value : acc;
@@ -32,10 +64,7 @@ export async function startOrStopBoxes(boxIds, start) {
     .map((biid) => biid.instanceId)
     .filter((iid) => iid);
 
-  const client = new EC2Client({
-    profile: "dwmkerr",
-    region: "us-west-2",
-  });
+  const client = new EC2Client();
 
   //  Start or stop em.
   if (start) {
@@ -75,39 +104,4 @@ export async function startOrStopBoxes(boxIds, start) {
     });
     return newInstancesStates;
   }
-}
-
-export async function getBoxes() {
-  const client = new EC2Client({
-    profile: "dwmkerr",
-    region: "us-west-2",
-  });
-
-  const instancesResponse = await client.send(
-    new DescribeInstancesCommand({
-      IncludeAllInstances: true,
-      Filters: [
-        {
-          Name: "tag:boxes.boxid",
-          Values: ["*"],
-        },
-      ],
-    }),
-  );
-
-  const instances = instancesResponse.Reservations.flatMap((r) => r.Instances);
-
-  //  We filter out terminated boxes (otherwise we can get multiple boxes with
-  //  the same ID, e.g. if a user quickly terminates and recreates a box).
-  const validInstances = instances.filter((i) => i.State.Name !== "terminated");
-
-  const boxes = validInstances.map((i) => ({
-    boxId: getTagValOr(i.Tags, "boxes.boxid", null),
-    instanceId: i.InstanceId,
-    name: nameFromTags(i.Tags),
-    status: i.State.Name,
-    instance: i,
-  }));
-
-  return boxes;
 }
