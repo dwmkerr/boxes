fd29dd274ee7ecbed666882bee3762be98606d75
Dave Kerr <dwmkerr@gmail.com>
2024-01-20 21:13:39 +0100

    feat: (wip) detach volumes

---
+++ src/commands/stop.test.ts
@@ -0,0 +1,26 @@
+import { EC2Client, DescribeInstancesCommand, DescribeVolumesCommand } from "@aws-sdk/client-ec2";
+import { mockClient } from "aws-sdk-client-mock";
+import "aws-sdk-client-mock-jest";
+import { stop } from "./stop";
+
+import describeInstancesResponse from "../fixtures/aws-ec2-describe-instances.json";
+import describeVolumesTorrentBoxResponse from "../fixtures/aws-ec2-describe-volumes-torrent-box.json";
+import { BoxState } from "../box";
+
+describe("stop", () => {
+  test("can stop boxes", async () => {
+    //  Record fixtures with:
+    //  AWS_PROFILE=dwmkerr aws ec2 describe-instances --filters "Name=tag:boxes.boxid,Values=*" > ./src/fixtures/aws-ec2-describe-instances.json
+    //  aws ec2 describe-volumes --filters Name=attachment.instance-id,Values=i-08fec1692931e31e7 > ./src/fixtures/aws-ec2-describe-volumes-torrent-box.json
+    const ec2Mock = mockClient(EC2Client)
+      .on(DescribeInstancesCommand)
+      .resolves(describeInstancesResponse)
+      .on(DescribeVolumesCommand)
+      .resolves(describeVolumesTorrentBoxResponse);
+
+    await stop("torrentbox", true);
+
+    expect(ec2Mock).toHaveReceivedCommand(DescribeInstancesCommand);
+    expect(ec2Mock).toHaveReceivedCommand(DescribeVolumesCommand);
+  });
+});
--- src/commands/stop.ts
+++ src/commands/stop.ts
@@ -1,7 +1,12 @@
-import { EC2Client, StopInstancesCommand } from "@aws-sdk/client-ec2";
+import { DescribeVolumesCommand, EC2Client, StopInstancesCommand } from "@aws-sdk/client-ec2";
 import { TerminatingWarning } from "../lib/errors";
 import { getBoxes } from "../lib/get-boxes";
 import { awsStateToBoxState } from "../box";
+
+interface DetachedVolumes {
+  volumeId: string;
+  device: string;
+}
 
 export async function stop(boxId: string, detach: boolean) {
   //  Get the box, fail with a warning if it is not found.
@@ -20,6 +25,37 @@ export async function stop(boxId: string, detach: boolean) {
 
   //  Create an EC2 client.
   const client = new EC2Client({});
+
+  //  If we are going to detach, we need to get the state of each attached
+  //  volume so that we can store it in a tag on the box, which will be used
+  //  when we restart the box later to attach to the correct place.
+  //  TODO; actally, all of this should be in 'lib' in a dedicated set of
+  //  smaller functions...
+  if (detach) {
+    console.log("debug: instance will not be stopped, testing detach");
+    //
+    //  Get the volumes for the box.
+    const response = await client.send(
+      new DescribeVolumesCommand({
+        Filters: [
+          {
+            Name: "attachment.instance-id",
+            Values: [box.instanceId],
+          },
+        ],
+      }),
+    );
+
+    console.log(JSON.stringify(response, null, 2));
+
+    return {
+      boxId,
+      instanceId: box.instanceId,
+      currentState: awsStateToBoxState(box.instance?.State?.Name),
+      previousState: awsStateToBoxState(box.instance?.State?.Name),
+    };
+
+  }
 
   //  Send the 'stop instances' command. Find the status of the stopping
   //  instance in the respose.
